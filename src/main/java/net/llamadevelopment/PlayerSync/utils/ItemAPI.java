e7db1ffc3df629501627ecfad281f1abe135db66
lt-name <572413378@qq.com>
2020-07-23 10:16:53 +0800

    Save CompoundTag

--- src/main/java/net/llamadevelopment/PlayerSync/utils/ItemAPI.java
+++ src/main/java/net/llamadevelopment/PlayerSync/utils/ItemAPI.java
@@ -3,6 +3,9 @@ package net.llamadevelopment.PlayerSync.utils;
 import cn.nukkit.inventory.Inventory;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
+import com.j256.ormlite.stmt.query.In;
+
+import java.util.Base64;
 
 public class ItemAPI {
 
@@ -14,69 +17,39 @@ public class ItemAPI {
         return stringInv.toString().substring(0, stringInv.toString().length() - 1);
     }
 
-    // itemInfo,Lore,Enchantments
-    // name#slot#id#damage#count,lore1#lore2#lore2,id:level#id:level
+    // itemInfo
+    // slot:id:damage:count:CompoundTag(base64)
 
     public static String toString(int slot, Item item) {
-        StringBuilder sb = new StringBuilder();
-        String customName = item.getCustomName().replace("#", "")
-                .replace("/", "")
-                .replace(",", "");
-        sb.append(customName).append("#");
-        sb.append(slot).append("#");
-        sb.append(item.getId()).append("#");
-        sb.append(item.getDamage()).append("#");
-        sb.append(item.getCount()).append(",");
-
-        // #,/
-
-        for (String lore : item.getLore()) {
-            String replacedLore = lore.replace("#", "")
-                    .replace("/", "")
-                    .replace(",", "");
-            sb.append(replacedLore).append("#");
-        }
-        sb.append(",");
-
-        for (Enchantment enchantment : item.getEnchantments()) {
-            sb.append(enchantment.getId())
-                    .append(":")
-                    .append(enchantment.getLevel())
-                    .append("#");
-        }
-        return sb.toString();
+        return slot + ":" + item.getId() + ":" + item.getDamage() + ":" +
+                (item.hasCompoundTag() ? bytesToBase64(item.getCompoundTag()) : "not");
     }
 
-    // itemInfo,Lore,Enchantments
-    // name#slot#id#damage#count,lore1#lore2#lore2,id:level#id:level
+    public static String bytesToBase64(byte[] src) {
+        if (src == null || src.length <= 0) {
+            return "not";
+        }
+        return Base64.getEncoder().encodeToString(src);
+    }
+
+    // itemInfo
+    // slot:id:damage:count:CompoundTag(base64)
 
     public static ItemWithSlot fromString(String itemString) throws NumberFormatException {
-        String[] info = itemString.split(",");
-        String[] general = info[0].split("#");
-        String name = general[0];
-        int slot = Integer.parseInt(general[1]);
-        int itemid = Integer.parseInt(general[2]);
-        int damage = Integer.parseInt(general[3]);
-        int count = Integer.parseInt(general[4]);
-        Item item = Item.get(itemid, damage, count);
-        if (name != null && !name.equals("")) {
-            item.setCustomName(name);
-        }
-        try {
-            String[] lores = info[1].split("#");
-            if (lores.length != 0) {
-                item.setLore(lores);
-            }
-        } catch (IndexOutOfBoundsException ignored) { }
-        try {
-            String[] enStrings = info[2].split("#");
-            for (String encString : enStrings) {
-                String[] eInfo = encString.split(":");
-                Enchantment enchantment = Enchantment.get(Integer.parseInt(eInfo[0])).setLevel(Integer.parseInt(eInfo[1]));
-                item.addEnchantment(enchantment);
-            }
-        } catch (IndexOutOfBoundsException ignored) { }
+        String[] info = itemString.split(":");
+        int slot = Integer.parseInt(info[0]);
+        Item item = Item.get(Integer.parseInt(info[1]),
+                Integer.parseInt(info[2]),
+                Integer.parseInt(info[3]),
+                base64ToBytes(info[4]));
         return new ItemWithSlot(slot, item);
+    }
+
+    public static byte[] base64ToBytes(String hexString) {
+        if (hexString == null || hexString.equals("")) {
+            return null;
+        }
+        return Base64.getDecoder().decode(hexString);
     }
 
 }
